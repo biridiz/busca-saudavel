5682210cd0e94f1b5d701c5313aa53194c07c089
Lucas Mahle <lucassmahle@gmail.com>
2021-01-26 13:06:32 -0300

    Friend list

---
+++ repositories/amizade.repository.js
@@ -0,0 +1,17 @@
+var ObjectId = require('mongoose').Types.ObjectId;
+
+const BaseRepository = require('./base.repository');
+const Amizade = require('../models/amizade.model');
+
+module.exports = class AmizadeRepository extends BaseRepository {
+    constructor() {
+        super(Amizade);
+    }
+
+    findByPerson(personId) {
+        return this.model.find({
+            pessoa1: personId
+        })
+            .populate('pessoa2');
+    }
+}
